6e4059a7c2ebf269b0b9dd52c0d2e43f9a3a143f
zoujie <zoujie@zoujiedeMini.AirDream>
2017-12-15 10:42:10 +0800

    demo init

---
+++ ZJCityPickerViewController/ZJCityPickerViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJCityPickerViewController
+//
+//  Created by zoujie on 2017/12/15.
+//  Copyright © 2017年 zoujie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZJCityPickerViewController/ZJCityPickerViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZJCityPickerViewController
+//
+//  Created by zoujie on 2017/12/15.
+//  Copyright © 2017年 zoujie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
